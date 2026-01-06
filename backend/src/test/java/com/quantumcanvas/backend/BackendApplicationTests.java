a03f513298e9825026b3b5a55ff97f694572b450
ricky <71623014+ricky4116@users.noreply.github.com>
2026-01-06 20:01:38 +0900

    Full project upload: backend and frontend

---
+++ backend/src/test/java/com/quantumcanvas/backend/BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.quantumcanvas.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
